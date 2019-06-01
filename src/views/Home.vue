0756483c19508268c174ab986aa7c1da0e4e6a4d
github tentoya <to.to.ten70@gmail.com>
2019-06-01 23:56:34 +0900

    SideNav.vueの追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="home">
-    <Addresses/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import Addresses from '@/components/Addresses.vue'
-export default {
-  name: 'home',
-  components: {
-    Addresses
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
